f89321948f47ae30307d79bb5b5e19016a21212f
jonmentesmaestras <jon.mosquera@gmail.com>
2026-03-01 12:47:00 -0500

    cambios en las keywords

--- background.js
+++ background.js
@@ -2,8 +2,6 @@
 
 chrome.action.onClicked.addListener(function (e) { chrome.windows.getCurrent(function (e) { parentWindowId = e.id }), chrome.windows.create({ url: chrome.runtime.getURL("popup.html?tabid=" + encodeURIComponent(e.id) + "&url=" + encodeURIComponent(e.url)), type: "popup", width: 720, height: 650 }) });
 
-chrome.action.onClicked.addListener(function (e) { chrome.windows.getCurrent(function (e) { parentWindowId = e.id }), chrome.windows.create({ url: chrome.runtime.getURL("popup.html?tabid=" + encodeURIComponent(e.id) + "&url=" + encodeURIComponent(e.url)), type: "popup", width: 720, height: 650 }) });
-
 chrome.runtime.onMessageExternal.addListener(
     function (request, sender, sendResponse) {
         console.log("Mensaje recibido de Load FB Ads Library. Ejecutando scraper...");
@@ -29,6 +27,7 @@ chrome.runtime.onMessageExternal.addListener(
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "monitorAndReopen") {
         const targetTabId = request.tabId;
+        const nextUrl = request.nextUrl;
 
         chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo, tab) {
             if (tabId === targetTabId && changeInfo.status === "complete") {
@@ -45,6 +44,12 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                 }, 3000);
             }
         });
+
+        if (nextUrl) {
+            chrome.tabs.update(targetTabId, { url: nextUrl });
+        }
+
         sendResponse({ status: "monitoring" });
+        return true;
     }
 });
